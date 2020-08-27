cd6552d4db94cf598f1ae0cc8d70e651e4039b00
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-08-27 19:19:15 +0530

    Create P2.c

---
+++ P2.c
@@ -0,0 +1,18 @@
+//Implement heirarchical file structure
+#include<stdio.h>
+struct Dir
+{
+    char files[20][100] ;
+    char subdirs[20][100];
+    int file_count;
+    int subdir_count;
+}
+void console()
+{
+  System.out.println("")
+}
+void main()
+{
+  console();
+  sdffs
+}
