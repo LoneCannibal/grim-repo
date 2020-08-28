2bba4754e2da69dd557a1ca07ea04b6e227e0ab1
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-08-28 12:20:51 +0530

    Console accept line is working

--- P2.c
+++ P2.c
@@ -1,6 +1,8 @@
 //Implement heirarchical file structure
 #include<stdio.h>
 #include<string.h>
+char path[200];
+void console();
 struct Dir
 {
     char dirname[200];
@@ -9,21 +11,19 @@ struct Dir
     int file_count;
     int subdir_count;
 };
-char path[200]="";
+struct Dir d[20];
 void console()
 {
-  printf("%s",path);
+  printf("%s> ",path);
   char command[250];
   scanf("%s",command);
 }
 void main()
 {
-  struct Dir d[20];
-  strcpy("Console/root",d[0].dirname);
+  strcpy(d[0].dirname,"Console/root");
   d[0].file_count=0;
   d[0].subdir_count=0;
-  strcpy(d[0].dirname,path);
-  printf("REACHED");
+  strcpy(path,d[0].dirname);
   console();
 
 }
