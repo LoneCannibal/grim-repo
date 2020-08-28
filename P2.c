77484bc92be9710313fb722942b67051fb398119
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-08-28 12:04:44 +0530

    Working on printing the first line

--- P2.c
+++ P2.c
@@ -1,18 +1,29 @@
 //Implement heirarchical file structure
 #include<stdio.h>
+#include<string.h>
 struct Dir
 {
-    char files[20][100] ;
-    char subdirs[20][100];
+    char dirname[200];
+    char files[20][200] ;
+    char subdirs[20][200];
     int file_count;
     int subdir_count;
-}
+};
+char path[200]="";
 void console()
 {
-  System.out.println("")
+  printf("%s",path);
+  char command[250];
+  scanf("%s",command);
 }
 void main()
 {
+  struct Dir d[20];
+  strcpy("Console/root",d[0].dirname);
+  d[0].file_count=0;
+  d[0].subdir_count=0;
+  strcpy(d[0].dirname,path);
+  printf("REACHED");
   console();
-  
+
 }
