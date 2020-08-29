27749ca5150e8c84f8b87ede5069e6c482b35adf
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-08-29 13:45:24 +0530

    make_directory fuction possibly finished

--- P2.c
+++ P2.c
@@ -2,37 +2,105 @@
 #include<stdio.h>
 #include<string.h>
 char path[200];
+char command[50];
+int current_dir_id;
 void console();
 struct Dir
 {
     char dirname[200];
     char files[20][200] ;
     char subdirs[20][200];
+    int parent_id;
     int file_count;
     int subdir_count;
+    int dir_id;
 };
-struct Dir d[20];
+struct Dir d[30];
+
+
+void make_directory(int i)
+{
+  char dirname1[200];
+  int j=0;
+  while(command[i]!='\0')
+  {
+    dirname1[j]=command[i];
+    j++;
+    i++;
+  }
+  for(int i=0;i<30;i++)
+  {
+    if(strcmp(d[i].dirname,"\0")==0)
+    {
+      strcpy(d[i].dirname,dirname1);
+      d[i].dir_id=i;
+      d[i].file_count=0;
+      d[i].subdir_count=0;
+      d[i].parent_id=current_dir_id;
+      printf("New directory "%s" created.\n",d[i].dirname);
+      console();
+    }
+  }
+  printf("NO SPACE FOR NEW DIRECTORY\n");
+  console();
+
+}
+
+
+
+void change_directory(int i)
+{
+  char dirname1[200];
+  int j=0;
+  while(command[i]!='\0')
+  {
+    dirname1[j]=command[i];
+    j++;
+    i++;
+  }
+  //NEED TO ADD MORE STUFF HERE
+  console();
+}
+
+
 void console()
 {
   printf("%s> ",path);
-  char command[250];
+  fflush(stdin);
+  gets(command);
   char cmdword[20];
-  scanf("%s",command);
   int i=0;
   while(command[i]!=' ')
   {
     cmdword[i]=command[i];
     i++;
+
   }
   i++;
-  printf("%s\n",cmdword);
+
+
+  if(strcmp(cmdword,"cd")==0)
+    change_directory(i);
+  else if(strcmp(cmdword,"mkdir")==0)
+    make_directory(i);
+
+  else
+  {
+    printf("Unrecognised command\n");
+    console();
+  }
 }
-void main()
+
+
+int main()
 {
   strcpy(d[0].dirname,"Console/root");
   d[0].file_count=0;
   d[0].subdir_count=0;
+  d[0].parent_id=-1;
+  d[0].dir_id=0;
   strcpy(path,d[0].dirname);
+  current_dir_id=0;
   console();
-
+  return 0;
 }
