416c42452292ebfd044f6533e1d2038b318e0dea
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-09-08 22:47:45 +0530

    Started work on single level directory

--- P1.c
+++ P1.c
@@ -1,2 +1,16 @@
 //Implementing single level directory structure
 #include<stdio.h>
+#include<string.h>
+str dir[50][50];
+str dir_name[50];
+void menu();
+{
+
+}
+int main()
+{
+  menu();
+  printf("Enter the name of directory ");
+  scanf("%s",dir_name);
+  return 0;
+}
