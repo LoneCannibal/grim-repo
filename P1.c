a3588d9b4644d464f760803b6fd38198159d25d8
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-09-08 23:14:46 +0530

    Second revision. Fixed a few silly errors

--- P1.c
+++ P1.c
@@ -1,12 +1,12 @@
 //Implementing single level directory structure
 #include<stdio.h>
 #include<string.h>
-str files[50][50];
-str dir_name[50];
+char files[50][50];
+char dir_name[50];
 int search()
 {
   printf("Enter name of the file: ");
-  str filname[50];
+  char filename[50];
   scanf("%s",filename);
   for(int i=0;i<50;i++)
     if(strcmp(filename,files[i])==0)
@@ -18,43 +18,46 @@ void menu()
   printf("\n1.Insert file\n2.Search file\n3.Delete file\n4.Display contents\n0.EXIT\nCHOOSE OPTION: ");
   int option;
   scanf("%d",&option);
-  case 1: for(int i=0;i<50;i++)
-          {
-            if(strcmp(files[i],"\0")==0)
+  switch(option)
+  {
+    case 1: for(int i=0;i<50;i++)
             {
-              printf("Enter name of the file: ");
-              scanf("%s",files[i]);
-              printf("FILE CREATED");
+              if(strcmp(files[i],"\0")==0)
+              {
+                printf("Enter name of the file: ");
+                scanf("%s",files[i]);
+                printf("FILE CREATED");
+                menu();
+              }
+            }
+            printf("NO MORE SPACE");
+            break;
+    case 2: if(search()!=-1)
+              {
+                printf("FILE FOUND");
+                menu();
+              }
+            printf("FILE NOT FOUND");
+            break;
+    case 3: int s=search();
+            if(s!=-1)
+            {
+              files[s]="\0";
+              printf("FILE DELETED");
               menu();
             }
-          }
-          printf("NO MORE SPACE");
-          break;
-  case 2: if(search()!=-1)
-            {
-              printf("FILE FOUND");
-              menu();
-            }
-          printf("FILE NOT FOUND");
-          break;
-  case 3: int s=search();
-          if(s!=-1)
-          {
-            files[s]="\0";
-            printf("FILE DELETED");
-            menu();
-          }
-          printf("FILE DOES NOT EXIST");
-          break;
+            printf("FILE DOES NOT EXIST");
+            break;
 
-  case 4: printf("Contents of Directory %s",dir_name);
-          for(int i=0;i<50;i++)
-          {
-            if(strcmp(filename,"\0")!=0)
-              printf("%s\n",files[i]);
-          }
-          break;
-  default: menu();
+    case 4: printf("Contents of Directory %s",dir_name);
+            for(int i=0;i<50;i++)
+            {
+              if(strcmp(files[i],"\0")!=0)
+                printf("%s\n",files[i]);
+            }
+            break;
+    default: menu();
+  }
 
 
 }
