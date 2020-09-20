c43c207efb8d77f2fe8a0ef2576dfe41e2503acf
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-09-20 21:01:37 +0530

    Program is possibly completed and tested working.

--- Banker_Algorithm.c
+++ Banker_Algorithm.c
@@ -3,11 +3,28 @@
 #include<stdlib.h>
 #include<stdbool.h> 
 char colours[5][10]={"[0;31m","[0;32m","[0;33m","[0;34m","[0;36m"};
-int number_of_processes,resource_types,allocated[20][5],max[20][5],total_resources[5];
+int number_of_processes,resource_types,not_enough_resources=0,allocated[20][5],max[20][5],total_resources[5];
 bool finished[20];
+void execute(int i)
+{
+    for(int j=0;j<resource_types;j++)
+        total_resources[j]=total_resources[j]+allocated[i][j];
+}
+bool finished_execution()
+{
+    not_enough_resources++;
+    if(not_enough_resources>number_of_processes+1)
+    {
+        printf("\n\n\033[1;31mFATAL ERROR OCCURED: NOT ENOUGH RESOURCES\n");
+        exit(0);
+    }
+    for(int i=0;i<number_of_processes;i++)
+        if(finished[i]==false)
+            return false;
+    return true;
+}
 void main()
 {
-    
     printf("\033[1;36mHow many processes are running? ");
     scanf("%d",&number_of_processes);
     printf("Enter the number of resource types ");
@@ -17,7 +34,7 @@ void main()
         printf("\033[1;31mTOO MANY RESOURCE TYPES\n");
         exit(0);
     }
-    printf("Enter the resource allocation fo each process");
+    printf("Enter the  allocated resources fo each process");
     for(int i=0;i<number_of_processes;i++)
     {
         printf("\033[0;35m\n\nPROCESS NUMBER:%d\n\n",i);
@@ -43,23 +60,30 @@ void main()
         printf("\033%sRESOURCE NUMBER %d->",colours[i],i);
         scanf("%d",&total_resources[i]);
     }
-    bool possible=true;
+    
     while(!finished_execution())
     {
         for(int i=0;i<number_of_processes;i++)
         {
+            bool possible=true;
             for(int j=0;j<resource_types;j++)
             {
-                if(allocated[i][j]+max[i][j]>total_resources[j])
+                
+                if(max[i][j]-allocated[i][j]>total_resources[j] && finished[i]==false)
                     possible=false;
             }
             if(possible==true)
             {
                 execute(i);
-                printf("\033[1;36mPROCESS %d FINISHED EXECUTION",i);
+                if(finished[i]==false)
+                {
+                    printf("\033[1;36m PROCESS %d FINISHED EXECUTION. TOTAL RESOURCES=[",i);
+                    for(int k=0;k<resource_types;k++)
+                        printf("\033%s   %d",colours[k],total_resources[k]);
+                    printf("\033[1;36m   ]\n");
+                }
+                finished[i]=true;
             }
         }
     }
-
-
 }
