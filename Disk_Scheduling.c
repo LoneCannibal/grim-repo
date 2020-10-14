2aa59024921a129016884be83458bdb4206a0c71
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-10-14 21:01:20 +0530

    Did the selection part

--- Disk_Scheduling.c
+++ Disk_Scheduling.c
@@ -3,6 +3,13 @@
 #include<string.h>
 #include<stdlib.h>
 int locations[30],n,start,end,head_loc;
+void fcfs()
+{
+    printf("THE ORDER IS:\n%d->",head_loc);
+    for(int i=0;i<n;i++)
+        printf("%d->",locations[i]);
+    printf("END\n");
+}
 void menu()
 {
     int input;
@@ -14,22 +21,21 @@ void menu()
     {
         case 1:fcfs();
             break;
-        case 2:sstf();
+        //case 2:sstf();
             break;
-        case 3:scan();
+        //case 3:scan();
             break;
-        case 4:cscan();
+        //case 4:cscan();
             break;
         default:menu();
         menu();
     }
-        
 }
 void main()
 {
     char loc_string[100],start_end_string[10],temp[10];
     int i=0,j=0,k=0;
-    printf("Enter the start and end positions seperated by spaces: ");
+    printf("Enter the start and end positions of the disk seperated by spaces: ");
     fgets(start_end_string,10,stdin);
     while(start_end_string[i]!='\0')
     {
@@ -44,7 +50,7 @@ void main()
         i++;
     }
     end=atoi(temp);
-    if(end>start)
+    if(start>end)
     {
         printf("ERROR OCCURED\n");
         exit(0);
@@ -66,5 +72,6 @@ void main()
         i++;
     }
     locations[j]=atoi(temp);
+    n=j+1;
     menu();
 }
