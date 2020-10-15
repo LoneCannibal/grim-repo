31c6b08c4409e876b3687868c251d29bf3f0f54b
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-10-15 14:48:26 +0530

    scan working

--- Disk_Scheduling.c
+++ Disk_Scheduling.c
@@ -12,6 +12,17 @@ bool completed()
             return false;
     return true;
 }
+void sort()
+{
+    for(int i=0;i<n-1;i++)
+        for(int j=0;j<n-i-1;j++)
+            if(locations[j]>locations[j+1])
+            {
+                int temp=locations[j];
+                locations[j]=locations[j+1];
+                locations[j+1]=temp;
+            }
+ }
 void fcfs()
 {
     printf("THE ORDER IS:\n%d->",head_loc);
@@ -39,13 +50,56 @@ void sstf()
 }
 void scan()
 {
-    printf("Work in Progress");
+    sort();
+    printf("HEAD IS MOVING FROM L TO R\nTHE ORDER IS:\n%d->",head_loc);
+    int i=0,temp=-1;
+    while(locations[i]<head_loc)
+        i++;
+    temp=i;
+    while(i!=n)
+    {
+        printf("%d->",locations[i]);
+        i++;
+    }
+    printf("%d->",end);
+    i=temp;
+    if(temp-1>=0)
+        i=temp-1;
+    while(i>=0)
+    {
+        printf("%d->",locations[i]);
+        i--;
+    }
+    
 }
 
 void menu()
 {
-    int input;
-    printf("\n\nEnter the head location: ");
+    char loc_string[100],temp[10];
+    int input,i=0,j=0,k=0;
+    for(int q=0;q<30;q++)
+        served[q]=false;
+    printf("\n\nEnter the end position of the disk: ");
+    scanf("%d",&end);
+    printf("\nEnter the locations seperated by spaces: ");
+    fgets(loc_string,100,stdin);//Extra statement because of strange error
+    fgets(loc_string,100,stdin);
+    while(loc_string[i]!='\0')
+    {
+        if(loc_string[i]==' ')
+        {
+            sscanf(temp, "%d", &locations[j]);
+            strcpy(temp,"");
+            k=0;
+            j++;
+        }
+        else
+            temp[k++]=loc_string[i];
+        i++;
+    }
+    sscanf(temp, "%d", &locations[j]);
+    n=j+1;
+    printf("Enter the head location: ");
     scanf("%d",&head_loc);  
     printf("\n1.FCFS\n2.SSTF\n3.SCAN\n4.CSCAN\nCHOOSE ALGORITHM: ");
     scanf("%d",&input);
@@ -67,29 +121,5 @@ void menu()
 }
 void main()
 {
-    char loc_string[100],temp[10];
-    int i=0,j=0,k=0;
-    strcpy(temp,"");
-    printf("Enter the end position of the disk: ");
-    scanf("%d",&end);
-    printf("\nEnter the locations seperated by spaces: ");
-    fgets(loc_string,100,stdin);//Extra statement because of strange error
-    fgets(loc_string,100,stdin);
-    i=0,j=0,k=0;
-    while(loc_string[i]!='\0')
-    {
-        if(loc_string[i]==' ')
-        {
-            sscanf(temp, "%d", &locations[j]);
-            strcpy(temp,"");
-            k=0;
-            j++;
-        }
-        else
-            temp[k++]=loc_string[i];
-        i++;
-    }
-    sscanf(temp, "%d", &locations[j]);
-    n=j+1;
     menu();
 }
