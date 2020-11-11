4620e797022e7039a6b8db03241c5e7516bb424d
LoneCannibal <you@example.com>
2020-11-11 20:45:09 +0530

    Made a few changes

--- Producer_Consumer.c
+++ Producer_Consumer.c
@@ -1,15 +1,39 @@
 //Program to provide a solution for producer consumer problem
 #include <unistd.h>
-#include <sys/types.h>
 #include <stdio.h>
+#include<string.h>
+#include <stdlib.h>
+#include<pthread.h>
+int i=0;
+int buffer[100];
+void *consumer(void *var)
+{
+    for(int j=0;j<10;j++)
+        printf("THREAD 1 %d\n",i++);
+    return NULL;
+}
+void *producer(void *var)
+{
+    for(int j=0;j<10;j++)
+        printf("THREAD 2 %d\n",i++);
+    return NULL;
+}
 int main()
 {
-    int i=0;
-    pid_t pid;
-    for(int i=0;i<10;i++)
+    pthread_t thread_id1,thread_id2;
+    printf("Enter the elements in the buffer seperated by spaces ");
+    char c[200];
+    fgets(c,200,stdin);
+    char* token=strtok(c," ");
+    while(token!=NULL)
     {
-        pid=fork();    
-        printf("%d   %d\n",i,pid);
+        sscanf(token,"%d",&buffer[i++]);
+        strtok(NULL," ");
     }
+    pthread_create(&thread_id1, NULL, consumer, NULL);
+    pthread_create(&thread_id2, NULL, producer, NULL); 
+    pthread_join(thread_id1,NULL);
+    pthread_join(thread_id2,NULL);
+    pthread_exit(NULL); 
     return 0;
 }
