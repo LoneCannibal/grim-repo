6105a29f3ec27d8fd6899570ca846c7990cc53b8
LoneCannibal <you@example.com>
2020-11-25 20:18:13 +0530

    Input file data storage works

--- Assembler.c
+++ Assembler.c
@@ -1,37 +1,45 @@
 //To implement one pass assembler
 #include<stdio.h>
 #include<stdlib.h>
-struct
+#include<string.h>
+struct line
 {
     char address[10];
     char label[10];
     char opcode[10];
     char operand[10];
 
-}line[100];
+} l[100];
 FILE *ptrin, *ptrinter,*ptrsymtab;
+void address_calc()
+{
+    
+}
 void assemble()
 {
-    char data[50];
-    while(ptrin!=EOF)
+    char data1[50],data2[50],data3[50];
+    int i=0;
+    while(feof(ptrin)==0)
     {
-        fscanf(ptrin,"%[^\n]",data);
-        char* token=strtok(data," ");
-        
-        printf("%s",data);
+       fscanf(ptrin,"%s%s%s",l[i].label,l[i].opcode,l[i].operand);
+       printf("%s %s %s",l[i].label,l[i].opcode,l[i].operand);   
     }
+    address_calc();
     fclose(ptrin);
+    fclose(ptrinter);
+    fclose(ptrsymtab);
 }
 
 int main()
 {
+   
     ptrin=fopen("./input.txt","r");
     ptrinter=fopen("./Intermediate.txt","rw");
     ptrsymtab=fopen("./symtab.txt","rw");
     if(ptrin==NULL||ptrinter==NULL||ptrsymtab==NULL)
     {
         printf("Unable to find required files\n");
-        exit(0);
+        exit(1);
     }
     assemble();
     return 0;
