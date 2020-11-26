65ad58924ee5f6d8bad5111aed1b80b2d16abdb2
LoneCannibal <you@example.com>
2020-11-26 19:38:35 +0530

    Intermediate file works

--- Assembler.c
+++ Assembler.c
@@ -4,7 +4,7 @@
 #include<string.h>
 struct line
 {
-    char address[10];
+    int address;
     char label[10];
     char opcode[10];
     char operand[10];
@@ -12,20 +12,7 @@ struct line
 } l[100];
 FILE *ptrin, *ptrinter,*ptrsymtab;
 int i=0;
-char *strrev(char *str)
-{
-  int q = strlen(str) - 1, j = 0;
-  char ch;
-  while (q > j)
-  {
-    ch = str[q];
-    str[q] = str[j];
-    str[j] = ch;
-    q--;
-    j++;
-  }
-  return str;
-}
+
 int hextodec(char a[10])
 {
     int dec=0;
@@ -38,39 +25,32 @@ int hextodec(char a[10])
     }
     return dec;
 }
-char* dectohex(int d)
-{
-    char h[10];
-    int j=0;
-    while(d>0)
-    {
-        int n=d%16;
-        if(n<10)
-          h[j++]=n+48;
-        else
-          h[j++]=(n-10+65);
-        
-        d=d/16;
-    }
-    char h2[10];
-    int k=0;
-    for(int i=0;i<strlen(h);i++)
-    {
-      if((h[i]>=48&&h[i]<59) || (h[i]>=65&&h[i<=91]))
-        h2[k++]=h[i];
-    }
-    return strrev(h2);
-}
+
 void address_calc()
 {
-    char addr[10];
+    int locctr;
     if(strcmp(l[0].opcode,"START")==0)
-        strcpy(addr,l[0].operand);
-    strcpy(l[0].address,addr);
-    for(int j=1;j<=i;j++)
+        locctr=hextodec(l[0].operand);
+    
+    for(int j=0;j<i;j++)
     {
-        
+            l[j].address=locctr;
+            fprintf(ptrinter,"%x %s %s %s\n",l[j].address,l[j].label,l[j].opcode,l[j].operand);
+            printf("%x %s %s %s\n",l[j].address,l[j].label,l[j].opcode,l[j].operand);
+    
+            if(strcmp(l[j].opcode,"RESW")==0)
+                locctr+=hextodec(l[j].operand)*3;
+            else if(strcmp(l[j].opcode,"RESB")==0)
+                locctr+=hextodec(l[j].operand);
+            else if(strcmp(l[j].opcode,"BYTE")==0)
+                locctr+=strlen(l[j].operand);
+            else if(strcmp(l[j].opcode,"START")==0)
+                 locctr=locctr;
+            else
+                locctr+=3;
+            
     }
+            
 }
 void assemble()
 {
@@ -78,8 +58,7 @@ void assemble()
     while(feof(ptrin)==0)
     {
        fscanf(ptrin,"%s%s%s",l[i].label,l[i].opcode,l[i].operand);
-       i++;
-       printf("%s %s %s",l[i].label,l[i].opcode,l[i].operand);   
+       i++; 
     }
     address_calc();
     fclose(ptrin);
@@ -91,8 +70,8 @@ int main()
 {
    
     ptrin=fopen("./input.txt","r");
-    ptrinter=fopen("./Intermediate.txt","rw");
-    ptrsymtab=fopen("./symtab.txt","rw");
+    ptrinter=fopen("./Intermediate.txt","w");
+    ptrsymtab=fopen("./symtab.txt","w");
     if(ptrin==NULL||ptrinter==NULL||ptrsymtab==NULL)
     {
         printf("Unable to find required files\n");
