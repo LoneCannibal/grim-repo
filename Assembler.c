2bb8f63a5d60445b6450bb32cc13563e678b9e5b
LoneCannibal <you@example.com>
2020-11-29 19:22:33 +0530

    Changed opcode to operator

--- Assembler.c
+++ Assembler.c
@@ -6,7 +6,7 @@ struct line
 {
     int address;
     char label[10];
-    char opcode[10];
+    char operator[10];
     char operand[10];
 } l[100];
 FILE *ptrin, *ptrinter,*ptrsymtab;
@@ -26,22 +26,22 @@ int hextodec(char a[10])
 void address_calc()
 {
     int locctr;
-    if(strcmp(l[0].opcode,"START")==0)
+    if(strcmp(l[0].operator,"START")==0)
         locctr=hextodec(l[0].operand);
     for(int j=0;j<i;j++)
     {
         l[j].address=locctr;
-        fprintf(ptrinter,"%x %s %s %s\n",l[j].address,l[j].label,l[j].opcode,l[j].operand);
+        fprintf(ptrinter,"%x %s %s %s\n",l[j].address,l[j].label,l[j].operator,l[j].operand);
         if(strcmp(l[j].label,"**")!=0 &&j!=0)
             fprintf(ptrsymtab,"%s %x\n",l[j].label,l[j].address);
-        printf("%x %s %s %s\n",l[j].address,l[j].label,l[j].opcode,l[j].operand);
-        if(strcmp(l[j].opcode,"RESW")==0)
+        printf("%x %s %s %s\n",l[j].address,l[j].label,l[j].operator,l[j].operand);
+        if(strcmp(l[j].operator,"RESW")==0)
             locctr+=hextodec(l[j].operand)*3;
-        else if(strcmp(l[j].opcode,"RESB")==0)
+        else if(strcmp(l[j].operator,"RESB")==0)
             locctr+=hextodec(l[j].operand);
-        else if(strcmp(l[j].opcode,"BYTE")==0)
+        else if(strcmp(l[j].operator,"BYTE")==0)
             locctr+=strlen(l[j].operand);
-        else if(strcmp(l[j].opcode,"START")!=0)
+        else if(strcmp(l[j].operator,"START")!=0)
             locctr+=3;
     }
 }
@@ -49,7 +49,7 @@ void assemble()
 {
     while(feof(ptrin)==0)
     {
-       fscanf(ptrin,"%s%s%s",l[i].label,l[i].opcode,l[i].operand);
+       fscanf(ptrin,"%s%s%s",l[i].label,l[i].operator,l[i].operand);
        i++; 
     }
     address_calc();
